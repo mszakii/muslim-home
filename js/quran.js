2fdc0b0d1506be615bd352868124d8943f547429
mszakii <learnmeprogramming@gmail.com>
2022-07-08 15:42:32 +0200

    Alfateha - Yasin

--- js/quran.js
+++ js/quran.js
@@ -1,8 +1,16 @@
 // page redirect quran
+sura.onclick = function () {
+  location.assign("sura/index.html");
+};
+
 sura1.onclick = function () {
   location.assign("sura1/index.html");
 };
 
 sura2.onclick = function () {
+  location.assign("sura2/index.html");
+};
+
+sura3.onclick = function () {
   location.assign("sura2/index.html");
 };
